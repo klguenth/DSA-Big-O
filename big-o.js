fdc64a371f94c729851b425556243ef90e66bb37
klguenth <klguenth@gmail.com>
2020-04-14 20:19:13 -0500

    add file

---
+++ big-o.js
@@ -0,0 +1,57 @@
+//Question 1
+//Part One
+Constant time O(1)
+//Part Two
+Logarithmic time O(log(n))
+
+//Question 2
+Constant time O(1)
+Constant time is typically found in algorithms that access array items or complete basic arithmetic operations.
+
+//Question 3
+Polynomial time O(n^k)
+Polynomial time is typically used for algorithms with nested loops.
+
+//Question 4
+Logarithmic time O(log(n))
+Logarithmic time is typically used for sorted array algorithms.
+
+//Question 5
+Constant time O(1)
+Constant time is used for algorithms that access array items.
+
+//Question 6
+Logarithmic time O(log(n))
+Logarithmic time is used for algorithms with sorted arrays.
+
+//Question 7
+Creates an array with the same number of array items as the number entered into the function.
+Linear time- as the running time is proportional to the input size.
+
+//Question 8
+Logarithmic time- as that is primarily used for sorted arrays. The running time increases slowly with larger inputs.
+
+//Question 9
+Constant time 0(1)
+Constant time algorithms are used to access array items or to perform basic arithmetic operations.
+
+//Question 10
+Constant time 0(1)
+Constant time algorithms are used for performing basic arithmetic.
+
+//Question 11
+Tower of Hanoi
+-Derive an algorithm
+-Implement algorithm using recursion
+-Given 5 disks, what do rods look like after 7 recursive calls
+-How many moves are needed to complete the puzzle with 3 disks/4 disks/5 disks?
+-What is the runtime of the algorithm
+
+//Question 12
+Solve previous exercises recursively
+
+//Question 13
+Identify time complexities of recursive exercises
+
+//Question 14
+Identify iterative exercise time complexity.
